eeb5fda634ffdef27111240e25ed4af029b5f2fc
shirley <tlxhong1108@gmail.com>
2025-07-19 09:02:48 -0700

    Python exercise 1

---
+++ main.py
@@ -0,0 +1,9 @@
+b="something"
+#print function and len function - this is length of the string
+print(len(b))
+
+# Input function is to ask user input
+name = input("Enter your username:")
+
+print("Welcome" + name)
+      
